fb70d9d4af9bdb24055773ce769776a661c428f2
Chris <c.r.zambito@gmail.com>
2023-12-17 15:18:02 -0500

    crystal about us

--- ghi/src/components/ourteam/OurTeam.jsx
+++ ghi/src/components/ourteam/OurTeam.jsx
@@ -179,6 +179,15 @@ const people = [
             "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
         bio: "",
     },
+    {
+        name: "Crystal Coakley",
+        role: "Claims Specialist",
+        phone: "",
+        email: "",
+        imageUrl:
+            "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+        bio: "",
+    },
 ];
 
 export default function OurTeam() {
