089f4f703cda71efdbf89e36270d1ce5c61e599d
Chris <c.r.zambito@gmail.com>
2023-12-17 15:16:42 -0500

    jemimah about us

--- ghi/src/components/ourteam/OurTeam.jsx
+++ ghi/src/components/ourteam/OurTeam.jsx
@@ -161,7 +161,15 @@ const people = [
             "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
         bio: "",
     },
-    
+    {
+        name: "Jemimah Cruz",
+        role: "Accounting Manager",
+        phone: "",
+        email: "",
+        imageUrl:
+            "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+        bio: "",
+    },
 ];
 
 export default function OurTeam() {
