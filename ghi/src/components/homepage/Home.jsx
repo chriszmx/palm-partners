2e02b32127c3bbb1eef3d860c60be8a94fe8f83f
Chris <c.r.zambito@gmail.com>
2024-03-15 14:15:28 -0400

    schema

--- ghi/src/components/homepage/Home.jsx
+++ ghi/src/components/homepage/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
                         }
                         `}
                 </script>
-
+                        {/* <!-- FAQ Schema --> */}
                 <script type="application/ld+json">
                     {`
                         {
