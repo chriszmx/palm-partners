427d5d6496d729473a34e998331946507c6f5167
Chris Zambito <c.r.zambito@gmail.com>
2024-03-22 13:42:58 -0400

    thermal imaging header

--- ghi/src/components/services/ThermalImaging.jsx
+++ ghi/src/components/services/ThermalImaging.jsx
@@ -3,10 +3,16 @@ import {
   AdjustmentsHorizontalIcon,
   EyeIcon
 } from '@heroicons/react/20/solid';
+import { Helmet } from 'react-helmet';
 
 export default function ThermalImaging() {
   return (
     <div className="relative bg-white">
+      <Helmet>
+        <title>Best Thermal Imager Solutions at Palm Partners | Unveil Hidden Issues</title>
+        <meta name="description" content="Rely on Palm PArtners for unmatched best thermal imager services in Florida. Expertly detect invisible problems, enhancing insurance claims with our detailed, high-tech analysis." />
+        <link rel="canonical" href="https://palmpts.com/services/thermal-imaging" />
+      </Helmet>
       <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
         <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
           <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
